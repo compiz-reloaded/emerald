bdcca74edc63e93ae5d0e9fa90a87696360bdf63
reggaemanu <reggaemanu>
2006-11-09 21:00:12 +0000

    emerald: avoid theme exportation to include suffixed ~ backup files

--- themer/main.c
+++ themer/main.c
@@ -460,7 +460,7 @@ static void export_theme(gchar * file)
     at = g_shell_quote(fn);
     g_free(fn);
     fn = g_shell_quote(file);
-    ot = g_strdup_printf("tar -czf %s -C %s ./",fn,at);
+    ot = g_strdup_printf("tar -czf %s -C %s ./ --exclude=*~",fn,at);
     if (!g_spawn_command_line_sync(ot,NULL,NULL,&ex,NULL) ||
             (WIFEXITED(ex)&&WEXITSTATUS(ex)))
     {
